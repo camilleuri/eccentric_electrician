01d77466a498db719f886fd5553f9ea90c34904b
camilleuri <123994913+camilleuri@users.noreply.github.com>
2025-04-08 16:35:43 -0400

    Add an example

--- examples.py
+++ examples.py
@@ -1,8 +1,9 @@
 from eccentric_electrician import Puzzle
+from random import randint
 
-# Example hardcoded solution to n = 5, lights on to start
+# Example hardcoded solution to n = 5, lights off to start
 
-p = Puzzle(5, "on")
+p = Puzzle(5, "off")
 
 p.check_puzzle()
 
@@ -35,3 +36,15 @@ p.flip_switch(0)
 
 p.game_state()
 p.check_puzzle()
+
+# Example bogosort-like solve, flip random switches until solved.
+
+size = 5
+i = 0
+
+e = Puzzle(size, starting_pos="on", verbose=False)
+while not e.check_puzzle():
+    e.flip_switch(randint(0, size-1))
+    i += 1
+
+print("Random flips required to solve:", i)
