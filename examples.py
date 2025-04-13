5b9cdc63946281a106ac31e92591a78899347b60
camilleuri <123994913+camilleuri@users.noreply.github.com>
2025-04-13 18:09:47 -0400

    Add mutually recursive solution
    Also modify random solution to use flip count tracker

--- examples.py
+++ examples.py
@@ -1,6 +1,29 @@
 from eccentric_electrician import Puzzle
 from random import randint
 
+# Example mutually recursive solution
+
+def turn_on(p, n):
+    if n == 0: p.flip_switch(0); return
+
+    turn_on(p, n-1)
+    if n > 1: turn_off(p, n-2)
+    p.flip_switch(n)
+    if n > 1: turn_on(p, n-2)
+    
+
+def turn_off(p, n):
+    if n == 0: p.flip_switch(0); return
+
+    if n > 1: turn_off(p, n-2)
+    p.flip_switch(n)
+    if n > 1: turn_on(p, n-2)
+    turn_off(p, n-1)
+
+p = Puzzle(10)
+turn_off(p, 9)
+print(p.flip_count)
+
 # Example hardcoded solution to n = 5, lights off to start
 
 p = Puzzle(5, "off")
@@ -39,12 +62,11 @@ p.check_puzzle()
 
 # Example bogosort-like solve, flip random switches until solved.
 
-size = 5
 i = 0
 
-e = Puzzle(size, starting_pos="on", verbose=False)
+e = Puzzle(5, starting_pos="on", verbose=False)
 while not e.check_puzzle():
-    e.flip_switch(randint(0, size-1))
+    e.flip_switch(randint(0, p.switch_count-1))
     i += 1
 
-print("Random flips required to solve:", i)
+print("Random tries required to solve:", i, "\nSwitches flipped successfully:", e.flip_count)
